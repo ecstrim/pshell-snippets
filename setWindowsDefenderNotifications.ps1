bd63c2cbca8fe8d4eb58cae9d1bdcb729676a1b9
ecstrim <1094666+ecstrim@users.noreply.github.com>
2022-07-20 13:57:59 +0200

    Create setWindowsDefenderNotifications.ps1

---
+++ setWindowsDefenderNotifications.ps1
@@ -0,0 +1,20 @@
+$RegPath1       = 'HKLM:\SOFTWARE\Microsoft\Windows Defender Security Center\Notifications'
+$KeyA1_Name     = 'DisableEnhancedNotifications'
+
+$RegPath2       = 'HKLM:\SOFTWARE\Microsoft\Windows Defender Security Center\Virus and threat protection'
+$KeyB1_Name     = 'SummaryNotificationDisabled'
+$KeyB2_Name     = 'NoActionNotificationDisabled'
+$KeyB3_Name     = 'FilesBlockedNotificationDisabled'
+
+# 0 - Enable Notifications
+# 1 - Disable Notifications
+$Value = 0
+
+# get keys
+#Get-ItemPropertyValue -Path $RegPath1 -Name $KeyA1_Name
+
+# Now set keys
+Set-ItemProperty -Path $RegPath1 -Name $KeyA1_Name -Value $Value -Force
+Set-ItemProperty -Path $RegPath2 -Name $KeyB1_Name -Value $Value -Force
+Set-ItemProperty -Path $RegPath2 -Name $KeyB2_Name -Value $Value -Force
+Set-ItemProperty -Path $RegPath2 -Name $KeyB3_Name -Value $Value -Force
