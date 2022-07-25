a2c9bdb20d7099d35729a44fdcf80b44c995b342
ecstrim <1094666+ecstrim@users.noreply.github.com>
2022-07-25 11:13:49 +0200

    Added A2 regkey

--- setWindowsDefenderNotifications.ps1
+++ setWindowsDefenderNotifications.ps1
@@ -1,5 +1,6 @@
 $RegPath1       = 'HKLM:\SOFTWARE\Microsoft\Windows Defender Security Center\Notifications'
-$KeyA1_Name     = 'DisableEnhancedNotifications'
+$KeyA1_Name     = 'DisableNotifications'
+$KeyA2_Name     = 'DisableEnhancedNotifications'
 
 $RegPath2       = 'HKLM:\SOFTWARE\Microsoft\Windows Defender Security Center\Virus and threat protection'
 $KeyB1_Name     = 'SummaryNotificationDisabled'
@@ -11,10 +12,15 @@ $KeyB3_Name     = 'FilesBlockedNotificationDisabled'
 $Value = 0
 
 # get keys
-#Get-ItemPropertyValue -Path $RegPath1 -Name $KeyA1_Name
+Get-ItemPropertyValue -Path $RegPath1 -Name $KeyA1_Name
+Get-ItemPropertyValue -Path $RegPath1 -Name $KeyA2_Name
+Get-ItemPropertyValue -Path $RegPath2 -Name $KeyB1_Name
+Get-ItemPropertyValue -Path $RegPath2 -Name $KeyB2_Name
+Get-ItemPropertyValue -Path $RegPath2 -Name $KeyB3_Name
 
 # Now set keys
 Set-ItemProperty -Path $RegPath1 -Name $KeyA1_Name -Value $Value -Force
+Set-ItemProperty -Path $RegPath1 -Name $KeyA2_Name -Value $Value -Force
 Set-ItemProperty -Path $RegPath2 -Name $KeyB1_Name -Value $Value -Force
 Set-ItemProperty -Path $RegPath2 -Name $KeyB2_Name -Value $Value -Force
 Set-ItemProperty -Path $RegPath2 -Name $KeyB3_Name -Value $Value -Force
