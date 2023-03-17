0ec512de50c0e2999b70d84f9a7d6c1ed1d183ad
ecstrim <1094666+ecstrim@users.noreply.github.com>
2023-03-17 18:28:48 +0100

    Update exportACL_hugeShare.ps1
    Parametrization and some minor updates

--- exportACL_hugeShare.ps1
+++ exportACL_hugeShare.ps1
@@ -1,48 +1,81 @@
 <#
 
-Used to export NTFS permissions for huge shares (no I mean really really huge)
-Directories only, no files
+Export ACLs of all the folders in a huge share
+Uses .Net libraries for performance and memory consumption reasons
 
-Uses .net libraries to keep memory consumption low
-
+.\exportACLS.ps1 -TargetPath '\\Server\Share\Dir' -DestinationPath 'C:\Temp'
 #>
+param (
+    # target share in \\server\share\dir format
+    [Parameter(Mandatory=$true)]
+    [string]$TargetPath,
 
-$directory = '\\MYSERVER\Myshare'
-
-$eDT = Get-Date
-$eDateTimeCustom = [STRING]$eDT.Year + "-" + $("{0:D2}" -f $eDT.Month) + "-" + $("{0:D2}" -f $eDT.Day) + "_" + $("{0:D2}" -f $eDT.Hour) + "." + $("{0:D2}" -f $eDT.Minute) + "." + $("{0:D2}" -f $eDT.Second)
-
-$csvPath = "C:\ACL-Export\NTFS_Permissions-$($eDateTimeCustom).csv"
-$logPath = "C:\ACL-Export\NTFS_Permissions_Log-$($eDateTimeCustom).txt"
-
-
+    # destination path
+    [Parameter(Mandatory=$true)]
+    [string]$DestinationPath
+)
 
 $StopWatch = [system.diagnostics.stopwatch]::startNew()
 $StopWatch.Start()
-$dirInfo = New-Object System.IO.DirectoryInfo($directory)
+
+$eDT = Get-Date
+$eDateTimeCustom = [STRING]$eDT.Year + "-" + $("{0:D2}" -f $eDT.Month) + "-" + $("{0:D2}" -f $eDT.Day) + "_" + $("{0:D2}" -f $eDT.Hour) + "." + $("{0:D2}" -f $eDT.Minute) + "." + $("{0:D2}" -f $eDT.Second)
+$csvPath = "$($DestinationPath)\NTFS_Permissions-$($eDateTimeCustom).csv"
+$logPath = "$($DestinationPath)\NTFS_Permissions_Log-$($eDateTimeCustom).txt"
+$logERR  = "$($DestinationPath)\NTFS_Permissions_Errors-$($eDateTimeCustom).txt"
+
+
+## here be dragons --------------------------------------------------------------------
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$message = "[$timestamp] Starting"
+Write-Host $message
+Add-Content -Path $logPath -Value $message
+
+
+$dirInfo = New-Object System.IO.DirectoryInfo($TargetPath)
 $folders = $dirInfo.GetDirectories("*", [System.IO.SearchOption]::AllDirectories)
 
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$message = "[$timestamp] Got the list of directories"
+Write-Host $message
+Add-Content -Path $logPath -Value $message
+
+# Needed for the progress bar
+$progressCounter = 0
+$totalCount = $folders.count
+
 foreach ($folder in $folders) {
+    $progressCounter++
+    Write-Progress -Activity 'Processing folders' -Status "Scanned: $progressCounter of $totalCount" -CurrentOperation $folder.FullName -PercentComplete(($progressCounter / $totalCount) * 100)
+
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $message = "[$timestamp] Processing folder $($folder.FullName)"
     #Write-Host $message
     Add-Content -Path $logPath -Value $message
     
     $acl = Get-Acl $folder.FullName
-    foreach ($ace in $acl.Access) {
-        if (($ace.IdentityReference -ne "NT AUTHORITY\System") -and ($ace.IdentityReference -notlike "Builtin\*")) {
-            $result = [ordered]@{
-                Folder = $folder.FullName
-                "User/Group" = $ace.IdentityReference
-                Permissions = $ace.FileSystemRights
-                AccessControlType = $ace.AccessControlType
-                IdentityReference = $ace.IdentityReference
-                IsInherited = $ace.IsInherited
-                InheritanceFlags = $ace.InheritanceFlags
-                PropagationFlags = $ace.PropagationFlags
+    if ( $acl ) {
+        foreach ($ace in $acl.Access) {
+            if (($ace.IdentityReference -ne "NT AUTHORITY\System") -and ($ace.IdentityReference -notlike "Builtin\*")) {
+                $result = [ordered]@{
+                    Folder = $folder.FullName
+                    "User/Group" = $ace.IdentityReference
+                    Permissions = $ace.FileSystemRights
+                    AccessControlType = $ace.AccessControlType
+                    IdentityReference = $ace.IdentityReference
+                    IsInherited = $ace.IsInherited
+                    InheritanceFlags = $ace.InheritanceFlags
+                    PropagationFlags = $ace.PropagationFlags
+                }
+                [PSCustomObject]$result | Export-Csv -Path $csvPath -Append -NoTypeInformation -Encoding UTF8  
             }
-            [PSCustomObject]$result | Export-Csv -Path $csvPath -Append -NoTypeInformation -Encoding UTF8
         }
+    } else {
+        $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $message = "[$timestamp] Error on $($folder.FullName)"
+        Add-Content -Path $logERR -Value $message
+        $message = "[$timestamp] $($_.Exception.message)"
+        Add-Content -Path $logERR -Value $message
     }
 }
 
