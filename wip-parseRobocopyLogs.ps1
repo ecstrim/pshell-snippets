7a0644112009f4b2f39b79d9c40074669a94bd41
ecstrim <1094666+ecstrim@users.noreply.github.com>
2023-05-26 10:13:29 +0200

    Update wip-parseRobocopyLogs.ps1

--- wip-parseRobocopyLogs.ps1
+++ wip-parseRobocopyLogs.ps1
@@ -7,26 +7,31 @@ $outputFile = "C:\path\to\output.txt"
 # Get all log files in the log directory
 $logFiles = Get-ChildItem -Path $logDir -Filter "*.log"
 
-# Initialize total failed files counter
+# Initialize total failed files and directories counters
 $totalFailedFiles = 0
+$totalFailedDirs = 0
 
 # Iterate through each log file
 foreach ($logFile in $logFiles) {
     # Parse log file for statistics
-    $stats = Select-String -Path $logFile.FullName -Pattern "^\s*Total\s*Copied\s*Skipped\s*Mismatch\s*FAILED\s*Extras" -Context 0, 15
+    $stats = Select-String -Path $logFile.FullName -Pattern "^\s*Dirs\s*:\s*\d+" -Context 0, 12
 
-    # Extract the number of failed files from the statistics
-    $failedFiles = ($stats.Context.PostContext[4] -split '\s+')[5]
+    # Extract the number of failed files and directories from the statistics
+    $failedDirs = ($stats.Context.PostContext[0] -split '\s+')[5]
+    $failedFiles = ($stats.Context.PostContext[1] -split '\s+')[5]
 
-    # Add the number of failed files to the total counter
-    $totalFailedFiles += $failedFiles
+    # Add the numbers of failed files and directories to the total counters
+    $totalFailedDirs += [int]$failedDirs
+    $totalFailedFiles += [int]$failedFiles
 
-    # Output log file name, statistics, and number of failed files to the output file
+    # Output log file name, statistics, and numbers of failed files and directories to the output file
     "`nLog file: $($logFile.Name)" | Out-File -Append $outputFile
     $stats.Line | Out-File -Append $outputFile
     $stats.Context.PostContext | Out-File -Append $outputFile
+    "Number of failed directories: $failedDirs" | Out-File -Append $outputFile
     "Number of failed files: $failedFiles" | Out-File -Append $outputFile
 }
 
-# Output total number of failed files to the output file
+# Output total number of failed directories and files to the output file
+"`nTotal number of failed directories: $totalFailedDirs" | Out-File -Append $outputFile
 "`nTotal number of failed files: $totalFailedFiles" | Out-File -Append $outputFile
