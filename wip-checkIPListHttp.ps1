274cf0577bfd84fe7328b6f46f7de01cbad46c02
Mihai Olaru <m.olaru@i2web.it>
2023-08-04 11:37:56 +0200

    4

--- wip-checkIPListHttp.ps1
+++ wip-checkIPListHttp.ps1
@@ -11,12 +11,12 @@ $ipAddresses = @()
 $ranges = 20..29 + 37..39 + 40..45 + 48..49 + 50..57
 
 # compose the ip list
-foreach ($number in $ranges) {
-    $ipAddresses += "${ipPrefix}.$number"
+foreach ($n in $ranges) {
+    $ipAddresses += "${ipPrefix}.$n"
 }
 
 foreach ($ip in $ipAddresses) {
-    Write-Host "Checking IP address: $ip" -ForegroundColor Cyan
+    Write-Host "IP: $ip" -ForegroundColor Cyan
     
     try {
         # 
@@ -24,19 +24,19 @@ foreach ($ip in $ipAddresses) {
         
         # check the request was successful
         if ($response.StatusCode -eq 200) {
-            Write-Host "Success: $($response.StatusCode)" -ForegroundColor Green
-            Write-Host "Headers for ${ip}:`n $($response.Headers)" -ForegroundColor Green
+            Write-Host "   Success: $($response.StatusCode)" -ForegroundColor Green
+            Write-Host "   Headers for ${ip}:`n $($response.Headers)" -ForegroundColor Green
         }
         else {
-            Write-Host "Failed with status code: $($response.StatusCode)" -ForegroundColor Yellow
+            Write-Host "   Failed with status code: $($response.StatusCode)" -ForegroundColor Yellow
         }
     }
     catch {
         if ($_.Exception.Message -like "*timeout*") {
-            Write-Host "Timeout connecting to $ip." -ForegroundColor Red
+            Write-Host "   Timeout connecting to $ip." -ForegroundColor Red
         }
         else {
-            Write-Host "Error connecting to $ip. Details: $_" -ForegroundColor Magenta
+            Write-Host "   Error connecting to $ip. Details: $_" -ForegroundColor Magenta
         }
     }
 }
